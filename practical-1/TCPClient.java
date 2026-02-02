31b5c67aad4a8e3e699c98e917f8aaa79d4eed03
Christian Asenov <caasenov@gmail.com>
2026-02-02 14:40:28 +0000

    add practical-1

---
+++ practical-1/TCPClient.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class TCPClient {
+
+	public TCPClient() {}
+
+	public static void main(String[] args) throws IOException {
+
+		// IP Addresses will be discussed in detail in lecture 4
+		String IPAddressString = "10.200.51.18";
+		InetAddress host = InetAddress.getByName(IPAddressString);
+
+		// Port numbers will be discussed in detail in lecture 5
+		int port = 2011;
+
+		// This is where we create a socket object
+		// That creates the TCP conection
+		System.out.println("TCPClient connecting to " + host.toString() + ":" + port);
+		Socket clientSocket = new Socket(host, port);
+
+		// Like files, we use readers and writers for convenience
+		BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+		// We can read what the server has said
+		String response = reader.readLine();
+		System.out.println("The server said : " + response);
+
+		// Close down the connection
+		clientSocket.close();
+	}
+}
